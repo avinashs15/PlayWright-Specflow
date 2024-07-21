86a821757b049f464fcf5090288db41eec699c2f
avinashs15 <avinashs15@gmail.com>
2024-07-22 01:06:17 +0200

    changed headless to true

--- Playwright-Specflow/src/Hooks/Hooks.cs
+++ Playwright-Specflow/src/Hooks/Hooks.cs
@@ -26,7 +26,7 @@ namespace Playwright_Specflow.src.Hooks
             var playwright = await Playwright.CreateAsync();
             _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
-                Headless = false,
+                Headless = true,
                 Channel = "chrome"
 
             });
